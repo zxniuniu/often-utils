7bd78b7c70a1e743573058ec6244d79070d4fbbf
Haicdust <hnuuhc@hotmail.com>
2021-04-18 11:41:51 +0800

    master

--- src/test/java/com/png/download/Test.java
+++ src/test/java/com/png/download/Test.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.haic.often.FilesUtils;
 import org.haic.often.Judge;
 import org.haic.often.StringUtils;
+import org.haic.often.URIUtils;
 import org.haic.often.Network.JsoupUtils;
 import org.haic.often.Network.NetworkFileUtils;
 import org.jsoup.nodes.Document;
@@ -19,17 +20,22 @@ public class Test {
 
 	private static final String proxyHost = App.proxyHost;
 	private static final int proxyPort = App.proxyPort;
+	private static final String already_usedid_filePath = App.yande_already_usedid_filePath; // 记录ID文件
 
 	public static void main(String[] args) {
 		String path = "F:\\Project\\IDEA\\PngAcc\\data\\Pictures\\yande";
 		List<File> downs = FilesUtils.iterateSuffixFiles(path, "down");
 		for (File down : downs) {
-			Document doc = JsoupUtils.connect("https://yande.re/post/show/" + StringUtils.extractRegex(down.getName(), "\\d+")).proxy(proxyHost, proxyPort).retry(true).GetDocument();
+			String imageid = StringUtils.extractRegex(down.getName(), "\\d+");
+			Document doc = JsoupUtils.connect("https://yande.re/post/show/" + imageid).proxy(proxyHost, proxyPort).retry(true).GetDocument();
 			String url = doc.selectFirst("id[png]").attr("href");
 			if (Judge.isNull(url)) {
 				url = doc.selectFirst("id[highres]").attr("href");
 			}
-			NetworkFileUtils.connect(url).proxy(proxyHost, proxyPort).retry(true).download(path);
+			int statusCode = NetworkFileUtils.connect(url).proxy(proxyHost, proxyPort).retry(true).download(path);
+			if (URIUtils.statusIsOK(statusCode)) {
+				ChildRout.WriteFileInfo(imageid, already_usedid_filePath);
+			}
 		}
 	}
 }
