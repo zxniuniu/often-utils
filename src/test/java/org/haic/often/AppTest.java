e38b4a8a36488ae97b1c52330018f662c569f3d0
HHC <huanghchen@hotmail.com>
2021-12-25 00:20:00 +0800

    master

--- src/test/java/org/haic/often/AppTest.java
+++ src/test/java/org/haic/often/AppTest.java
@@ -7,12 +7,13 @@ import org.haic.often.Network.NetworkFileUtils;
  */
 public class AppTest {
 
-    private static final String proxyHost = "127.0.0.1";
-    private static final int proxyPort = 7890;
+	private static final String proxyHost = "127.0.0.1";
+	private static final int proxyPort = 7890;
 
-    public static void main(String[] args) {
-        String url = "https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/QQ9.5.3.28008.exe";
-        NetworkFileUtils.connect(url).multithread(10).method(NetworkFileUtils.Method.MULTITHREAD).interval(100).retry(true).download("F:\\Downloads");
-    }
+	public static void main(String[] args) {
+		String url = "https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/QQ9.5.3.28008.exe";
+		NetworkFileUtils.connect(url).multithread(10).method(NetworkFileUtils.Method.MULTITHREAD).interval(10).retry(true).download("F:\\Downloads");
+		System.out.println("1");
+	}
 
 }
