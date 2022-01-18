9fb03322fdb67a8f7142b0ffad5ea18133b8fcb9
HHC <huanghchen@hotmail.com>
2022-01-19 00:33:32 +0800

    Tuple 元类增加至TenTuple

---
+++ src/main/java/org/haic/often/Tuple/SixTuple.java
@@ -0,0 +1,16 @@
+package org.haic.often.Tuple;
+
+/**
+ * @author haicdust
+ * @version 1.0
+ * @since 2022/1/19 0:16
+ */
+public class SixTuple<A, B, C, D, E, F> extends FiveTuple<A, B, C, D, E> {
+	public final F sixth;
+
+	public SixTuple(A a, B b, C c, D d, E e, F f) {
+		super(a, b, c, d, e);
+		sixth = f;
+	}
+	
+}
--- src/main/java/org/haic/often/Tuple/TupleUtil.java
+++ src/main/java/org/haic/often/Tuple/TupleUtil.java
@@ -21,4 +21,24 @@ public final class TupleUtil {
 		return new FiveTuple<>(a, b, c, d, e);
 	}
 
+	public static <A, B, C, D, E, F> SixTuple<A, B, C, D, E, F> tuple(A a, B b, C c, D d, E e, F f) {
+		return new SixTuple<>(a, b, c, d, e, f);
+	}
+
+	public static <A, B, C, D, E, F, G> SevenTuple<A, B, C, D, E, F, G> tuple(A a, B b, C c, D d, E e, F f, G g) {
+		return new SevenTuple<>(a, b, c, d, e, f, g);
+	}
+
+	public static <A, B, C, D, E, F, G, H> EightTuple<A, B, C, D, E, F, G, H> tuple(A a, B b, C c, D d, E e, F f, G g, H h) {
+		return new EightTuple<>(a, b, c, d, e, f, g, h);
+	}
+
+	public static <A, B, C, D, E, F, G, H, I> NineTuple<A, B, C, D, E, F, G, H, I> tuple(A a, B b, C c, D d, E e, F f, G g, H h, I i) {
+		return new NineTuple<>(a, b, c, d, e, f, g, h, i);
+	}
+
+	public static <A, B, C, D, E, F, G, H, I, J> TenTuple<A, B, C, D, E, F, G, H, I, J> tuple(A a, B b, C c, D d, E e, F f, G g, H h, I i, J j) {
+		return new TenTuple<>(a, b, c, d, e, f, g, h, i, j);
+	}
+
 }
