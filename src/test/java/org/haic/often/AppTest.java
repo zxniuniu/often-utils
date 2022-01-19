d3fb1d7c7bc100a50dc0ca9113b2416dcd306f68
HHC <huanghchen@hotmail.com>
2022-01-20 02:32:41 +0800

    .

--- src/test/java/org/haic/often/AppTest.java
+++ src/test/java/org/haic/often/AppTest.java
@@ -24,6 +24,7 @@ public class AppTest {
 		Iterator<Map.Entry<String, String>> entries2 = cookies2.entrySet().iterator();
 		while (entries2.hasNext()) {
 			Map.Entry<String, String> entry = entries2.next();
+			
 			System.out.println(entry.getKey() + " = " + entry.getValue());
 		}
 
